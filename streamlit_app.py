3c68a612c21334ac6926c34f778b61667432c1c1
Thomas Hughes <tom@rnw.co.uk>
2026-07-20 22:52:38 +0100

    Refactor requests import and API call location
    Moved the requests import and API call inside the function to improve code organization.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -31,6 +32,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -60,9 +63,3 @@ if ingredients_list:
     #if ingredients_string:
     #  session.sql(my_insert_stmt).collect()
 
-
-import requests
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
