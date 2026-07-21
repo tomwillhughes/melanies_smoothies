b1fe61bfc85705de09f38034649ac0923f658e6e
Thomas Hughes <tom@rnw.co.uk>
2026-07-21 10:34:16 +0100

    Refactor dataframe selection in streamlit_app.py
    Removed unnecessary select call for FRUIT_NAME.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write('The name on your smoothie will be', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON")))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
 
 # removed <.select(col('FRUIT_NAME'))> and replaced
 
