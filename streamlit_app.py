0d05b5953cd3373e5c26e5aa74c5a5136e78e5b1
Thomas Hughes <tom@rnw.co.uk>
2026-07-21 10:23:30 +0100

    Update dataframe query in streamlit_app.py
    Removed unnecessary selection of 'FRUIT_NAME' from the dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,10 @@ st.write('The name on your smoothie will be', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options")
+
+# removed <.select(col('FRUIT_NAME'))>
+
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
